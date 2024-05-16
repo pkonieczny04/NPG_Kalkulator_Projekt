a88d431c2227aabbc4ca708dcbd8ae1ac86e7221
dom3kkk <164013121+dom3kkk@users.noreply.github.com>
2024-05-16 12:34:23 +0200

    dodano funkcje odejmowania do pliku calculator.py

--- calculator.py
+++ calculator.py
@@ -4,9 +4,13 @@ import math
 class Calculator:
 
     def add(self, a, b):
+        
 
     def substract(self, a, b):
-
+        result = a - b
+        self._save_to_history(f"subtract({a}, {b}) = {result}")
+        return result
+    
     def multiply(self, a, b):
         result = a * b
         self._save_to_history(f"multiply({a}, {b}) = {result}")
