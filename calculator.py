c3d87b88011894ec65f51b64270b74d6c72f71c2
TMLMarJ <164010021+TMLMarJ@users.noreply.github.com>
2024-05-16 11:51:59 +0200

    Utworzono plik nagłówkowy do którego należy dodawać metody

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#Plik nagłówkowy
+class Calculator:
+
+    def add(self, a, b):
+
+    def substract(self, a, b):
+
+    def multiply(self, a, b):
+
+    def divide(self, a, b):
+
+    def power(self, a, b):
+
+    def sqrt(self, a):
+
+    
