d9435d5604972ef8b45f30ebd55524dd54581588
Piotr Konieczny <konieczny.piotr04@gmail.com>
2024-05-19 17:20:50 +0200

    Usunięcie prototypu save_to_history w celu wykonania testów

--- calculator.py
+++ calculator.py
@@ -2,22 +2,17 @@
 import math
 
 class Calculator:
-    def __init__(self):
-        self.history = []
 
     def addition(self, a, b) :
         result = a + b
-        self._save_to_history(f"add({a}, {b}) = {result}")
         return result
 
     def substract(self, a, b):
         result = a - b
-        self._save_to_history(f"subtract({a}, {b}) = {result}")
         return result
     
     def multiply(self, a, b):
         result = a * b
-        self._save_to_history(f"multiply({a}, {b}) = {result}")
         return result
     def divide(self, a, b):
         pass
