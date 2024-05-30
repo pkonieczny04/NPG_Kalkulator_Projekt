76c22aad4d277a2f14bf4773ea2da0f0e0884909
JakubKarabowicz <164009871+JakubKarabowicz@users.noreply.github.com>
2024-05-30 19:54:10 +0200

    stworzenie szkieletu testów string_calculator

---
+++ string_calculator_test.py
@@ -0,0 +1,24 @@
+class TestCalculatorEvaluateExpression(unittest.TestCase):
+  def test_evaluate_expression_simple(self):
+    pass
+
+  def test_evaluate_expression_complex(self):
+    pass
+
+  def test_evaluate_expression_with_complex_numbers(self):
+    pass
+  
+  def test_evaluate_expression_with_nested_complex_operations(self):     
+   pass
+  
+  def test_evaluate_expression_invalid(self):
+    pass
+  
+  
+  
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+        
